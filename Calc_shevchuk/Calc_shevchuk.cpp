958490cb0c38b08cdad7e6a23fc520bf7ae89b4a
Witcher-MTM <phantomit228@gmail.com>
2021-12-09 20:16:05 +0200

    add * and / funcs

--- Calc_shevchuk/Calc_shevchuk.cpp
+++ Calc_shevchuk/Calc_shevchuk.cpp
@@ -8,9 +8,22 @@ void Minus(int x, int y)
 {
     cout << x - y;
 }
+void Umnozhenie(int x, int y)
+{
+    cout << x * y;
+}
+void Delenie(int x, int y)
+{
+    cout << x / y;
+}
 int main()
 {
     Plus(6, 5);
     cout << "\n";
     Minus(12, 6);
+    cout << "\n";
+    Umnozhenie(3, 2);
+    cout << "\n";
+    Delenie(4, 2);
+    cout << "\n";
 }
