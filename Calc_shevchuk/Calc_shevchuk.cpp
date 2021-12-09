e813597197f8c0e9ad9b1413242e73e89e739382
Witcher-MTM <phantomit228@gmail.com>
2021-12-09 20:25:25 +0200

    add div by zero assert in / func

--- Calc_shevchuk/Calc_shevchuk.cpp
+++ Calc_shevchuk/Calc_shevchuk.cpp
@@ -1,48 +1,53 @@
 #include <iostream>
 using namespace std;
-void Plus(int x,int y)
+void Plus(int x, int y)
 {
-    cout<<x + y;
+	cout << x + y;
 }
 void Minus(int x, int y)
 {
-    cout << x - y;
+	cout << x - y;
 }
 void Umnozhenie(int x, int y)
 {
-    cout << x * y;
+	cout << x * y;
 }
 void Delenie(int x, int y)
 {
-    cout << x / y;
+	if (y == 0)
+		cout << "Division by zero...";
+	else
+	{
+		cout << x / y;
+	}
 }
-void More(int x,int y)
+void More(int x, int y)
 {
-    if (x > y)
-        cout << x;
-    else
-        cout << y;
+	if (x > y)
+		cout << x;
+	else
+		cout << y;
 
 }
 void Less(int x, int y)
 {
-    if (x < y)
-        cout << x;
-    else
-        cout << y;
+	if (x < y)
+		cout << x;
+	else
+		cout << y;
 
 }
 int main()
 {
-    Plus(6, 5);
-    cout << "\n";
-    Minus(12, 6);
-    cout << "\n";
-    Umnozhenie(3, 2);
-    cout << "\n";
-    Delenie(4, 2);
-    cout << "\n";
-    More(6, 7);
-    cout << "\n";
-    Less(9, 15);
+	Plus(6, 5);
+	cout << "\n";
+	Minus(12, 6);
+	cout << "\n";
+	Umnozhenie(3, 2);
+	cout << "\n";
+	Delenie(4, 2);
+	cout << "\n";
+	More(6, 7);
+	cout << "\n";
+	Less(9, 15);
 }
